1df34a35ab53277f51474de4550e6a749dcf4f0c
Matt Hough <matt@hough.tv>
2017-09-25 23:12:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,5 @@ require "pry"
 sentence = "Hi again, just making sure it's reversed!"
 
 def reverse_each_word(sentence)
-  sentence.split.collect do |word|
-    new_string = word.reverse
-    new_string.join(" ")
-  end
-  binding.pry
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
