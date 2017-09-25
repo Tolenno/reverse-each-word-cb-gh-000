0f0f95893b16c8ed17ac373df06a6fcc9f29842a
Matt Hough <matt@hough.tv>
2017-09-25 23:06:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ sentence = "Hi again, just making sure it's reversed!"
 def reverse_each_word(sentence)
   array = sentence.split
   new_string = ""
-  array.collect do |word|
+  array.each do |word|
     new_string << "#{word.reverse} "
   end
   new_string.rstrip
